eeab5d5fe40e23263bd5edc80e498d8c3ceb38f9
transistorski <transistorski@gmail.com>
2010-07-03 21:25:22 +0000

    Added handler for '3' (CALL FAR) and '5' (JMP FAR) 8086 snafu

--- test/okaytdiff.py
+++ test/okaytdiff.py
@@ -1,5 +1,4 @@
-
-#! /usr/bin/python
+#!/usr/bin/python
 
 import sys
 
@@ -11,24 +10,24 @@ for iter in lines_l:
 		qiter_l = qiter.split ()
 		invalid = qiter_l[2] == '???'
 		if invalid:
-			continue
+			continue # this is okay
 		elif qiter_l[2] == 'JMP':
 			far = qiter_l[3] == 'FAR'
 			if far and qiter_l[4] in "AX CX DX BX SP BP SI DI AH AL BH BL CH CL DH DL".split ():
-				continue
+				continue # this is okay
 			else: 
-				code = int (qiter_l[1], 16)
-				codeh = code / 0x100
-				codel = code % 0x100
+				code = int (qiter_l[1], 16) # two-byte instructions remain
+				opcode = code / 0x100		# insulate opcode
+				modrmbyte = code % 0x100        # insulate mod-r/m byte
 				mode = codel / 0x40
-				# instruction = codel / 8 - mode * 8
-				print "%x, %x, %x, %x" % (codeh, codel, mode, instruction)
+				instruction = (codel - mode * 0x40) / 8 # insulate the r/m bits, value enumerates instruction
+				print instruction
 				if codeh in [	0xFF, 
 						0xFE,
 						0xC5,
 						0xC4,
 						0x8D ] and mode == 3 and instruction in [3, 5]:
-					continue
+					continue # this is okay
 				else:
 					print >> sys.__stderr__, qiter 
 						
