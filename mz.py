038fb6d9d5bee458543042ececc188d252d0503c
transistorski <transistorski@gmail.com>
2010-07-04 00:35:23 +0000

    Added GPL headers

--- mz.py
+++ mz.py
@@ -1,3 +1,20 @@
+# Copyright 2009 Michael Heyeck
+# This file is part of dasm3.py.
+#
+# dasm3.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# 
+# dasm3.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with dasm3.py.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 import  struct
 
 class MzHeader:
